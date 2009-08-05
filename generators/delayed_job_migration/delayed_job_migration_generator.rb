33d648be7b029c860c7ac2fb0540bd1de880baca
Jason Whittle <jason.whittle@gmail.com>
2009-08-05 00:23:57 -0400

    Copied session_migration whole-cloth to create a passable delayed_job_migration generator.

---
+++ generators/delayed_job_migration/delayed_job_migration_generator.rb
@@ -0,0 +1,19 @@
+class DelayedJobMigrationGenerator < Rails::Generator::NamedBase
+	def initialize(runtime_args, runtime_options = {})
+		runtime_args << 'add_delayed_job_table' if runtime_args.empty?
+		super
+	end
+	
+	def manifest
+		record do |m|
+			m.migration_template 'migration.rb', 'db/migrate',
+				:assigns => { :delayed_job_table_name => default_delayed_job_table_name }
+		end
+	end
+	
+	protected
+	
+	def default_delayed_job_table_name
+		ActiveRecord::Base.pluralize_table_names ? 'delayed_job'.pluralize : 'delayed_job'
+	end
+end
